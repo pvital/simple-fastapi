1abd51f07ea928c9f5077b4f284f1f2ba43d8d12
Paulo Vital <paulo.vital@ibm.com>
2025-08-28 16:10:26 +0200

    feat: App with uv support.
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 
 Simple [FastAPI] app.
 
+## Prerequisites
+
+- [uv]
+
+## Running
+
+```shell
+uv run fastapi dev app.py
+```
+
 [FastAPI]: https://fastapi.tiangolo.com "FastAPI"
 [OpenShift]: https://www.redhat.com/en/technologies/cloud-computing/openshift "RedHat OpenShift"
+[uv]: https://docs.astral.sh/uv/ "uv"
---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from typing import Union
+
+import psutil
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+pid = os.getpid()
+print(pid)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    if not psutil.pid_exists(item_id):
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    # procs = {p.pid: p.info for p in psutil.process_iter(['name', 'username'])}
+    procs = {p.pid: p for p in psutil.process_iter()}
+    return {"item_id": item_id, "info": procs[item_id].as_dict()}
